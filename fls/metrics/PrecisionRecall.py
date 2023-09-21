124997445c41c041dd3ddd743d8a8efe11224f2b
Marco Jiralerspong <marcojira@gmail.com>
2023-09-20 22:07:28 -0400

    Batched Precision/Recall

--- fls/metrics/PrecisionRecall.py
+++ fls/metrics/PrecisionRecall.py
@@ -1,6 +1,10 @@
 """ PyTorch reimplementation from https://github.com/kynkaat/improved-precision-and-recall-metric """
+import math
 import torch
+
 from fls.metrics.Metric import Metric
+
+BATCH_SIZE = 10000
 
 
 class PrecisionRecall(Metric):
@@ -12,19 +16,30 @@ class PrecisionRecall(Metric):
         self.num_neighbors = num_neighbors
 
     def get_nn_dists(self, feat):
-        dists = torch.cdist(feat, feat)
-        dists = dists.topk(self.num_neighbors, dim=1, largest=False).values
-        return dists[:, -1]
+        dists = torch.zeros(feat.shape[0]).cuda()
+        for i in range(math.ceil(feat.shape[0] / BATCH_SIZE)):
+            start, end = i * BATCH_SIZE, (i + 1) * BATCH_SIZE
+            curr_dists = torch.cdist(feat[start:end], feat)
+            curr_dists = curr_dists.topk(
+                self.num_neighbors, dim=1, largest=False
+            ).values
+            dists[start:end] = curr_dists[:, -1]
+        return dists
 
     def pct_in_manifold(self, evaluated_feat, manifold_feat):
+        total_in_manifold = 0
         nn_dists = self.get_nn_dists(manifold_feat)
-        pairwise_dists = torch.cdist(evaluated_feat, manifold_feat)
 
-        comparison_tensor = nn_dists.unsqueeze(0).repeat(pairwise_dists.shape[0], 1)
+        for i in range(math.ceil(evaluated_feat.shape[0] / BATCH_SIZE)):
+            start, end = i * BATCH_SIZE, (i + 1) * BATCH_SIZE
+            pairwise_dists = torch.cdist(evaluated_feat[start:end], manifold_feat)
+            comparison_tensor = nn_dists.unsqueeze(0).repeat(pairwise_dists.shape[0], 1)
 
-        num_in_manifold = (pairwise_dists < comparison_tensor).sum(dim=1)
-        num_in_manifold = (num_in_manifold > 0).sum()
-        return num_in_manifold / evaluated_feat.shape[0]
+            num_in_manifold = (pairwise_dists < comparison_tensor).sum(dim=1)
+            num_in_manifold = (num_in_manifold > 0).sum()
+            total_in_manifold += num_in_manifold
+
+        return total_in_manifold / evaluated_feat.shape[0]
 
     def compute_metric(
         self,
