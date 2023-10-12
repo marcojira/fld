03f613810aac089cfaaa7537844afc8f42cc5e95
Marco Jiralerspong <marcojira@gmail.com>
2023-10-12 18:18:29 -0400

    Added baseline LL computation

--- fls/metrics/FLS.py
+++ fls/metrics/FLS.py
@@ -2,12 +2,17 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+from fls.utils import shuffle
 from fls.metrics.Metric import Metric
 from fls.MoG import preprocess_feat, MoG
 
 
+GEN_SIZE = 10000
+
+
 class FLS(Metric):
-    def __init__(self, eval_feat="test", baseline_LL=0):
+    def __init__(self, eval_feat="test", baseline_nll=None):
         super().__init__()
 
         # One of ("train", "test")
@@ -16,9 +21,18 @@ class FLS(Metric):
         self.name = f"FLS {eval_feat.title()}"
 
         # Corresponds to the likelihood of the test set under a MoG centered at half of the train set fit to the other half of the train set
-        self.baseline_LL = baseline_LL
+        self.baseline_nll = baseline_nll
+
+    def get_nll_diff(self, nll):
+        return (nll - self.baseline_nll) * 100
 
     def compute_metric(self, train_feat, test_feat, gen_feat):
+        if not self.baseline_nll:
+            self.get_baseline_nll(train_feat, test_feat)
+
+        if len(gen_feat) > GEN_SIZE:
+            gen_feat = shuffle(gen_feat, min(len(gen_feat), GEN_SIZE))
+
         """Preprocess"""
         train_feat, test_feat, gen_feat = preprocess_feat(
             train_feat, test_feat, gen_feat
@@ -37,4 +51,18 @@ class FLS(Metric):
         else:
             raise Exception(f"Invalid mode for FLS metric: {self.eval_feat}")
 
-        return nlls.mean().item()
+        nll = nlls.mean().item()
+        return self.get_nll_diff(nll)
+
+    def get_baseline_nll(self, train_feat, test_feat):
+        """Preprocess"""
+        train_feat, test_feat, _ = preprocess_feat(train_feat, test_feat, train_feat)
+
+        train_feat = shuffle(train_feat)
+
+        # Fit MoGs
+        split_size = min(GEN_SIZE, len(train_feat) // 2)
+        mog = MoG(train_feat[:split_size])
+        mog.fit(train_feat[split_size:])
+
+        self.baseline_nll = mog.get_dim_adjusted_nlls(test_feat).mean().item()
